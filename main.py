0e0cce1b281dad75386ff2ff7d911b752c301b82
Todd, Jacob <jtodd@verisk.com>
2024-07-30 19:59:23 -0500

    I added the random number creator and then used it to randomly select an item type.

---
+++ main.py
@@ -0,0 +1,6 @@
+from items import itemType, weapon, armor
+from randomizer import randomnumber
+
+
+
+print(itemType[randomnumber(0, 7)] + " " + weapon[0]);
---
+++ randomizer.py
@@ -0,0 +1,18 @@
+import random
+
+# generate rondom number between 1 and 10
+# print(random.randint(1, 10))
+# I want randomnumber to take two paramiters the min and the max
+# I then want the user to have ot set these values when the function is called
+
+# I defined the function
+def randomnumber(min, max):
+    return random.randint(min, max)
+
+
+# I called the function
+randomnumber(2, 5)
+
+# I printed the result
+print(randomnumber(0, 5))
+
