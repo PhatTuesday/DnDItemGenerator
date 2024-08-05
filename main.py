243249b6e399e32c6d9cd31239579d3d7409e67e
Todd, Jacob <jtodd@verisk.com>
2024-08-05 09:18:42 -0500

    Added Necklace list to item attributes tabel. Changed the print statements to overwrite an existing veriable and then main prints the answer.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import randomizer
 from itemTables import itemAttributes
 from randomizer import randomnumber
 
@@ -7,4 +8,6 @@ def return_item():
     return itemAttributes['itemType'][randomnumber(0, 7)]
 
 
-print(return_item())
+# print(return_item())
+
+print(randomizer.answer)
--- randomizer.py
+++ randomizer.py
@@ -1,6 +1,7 @@
 import random
 from itemTables import itemAttributes
 
+
 def randomnumber(min, max):
     return random.randint(min, max)
 
@@ -14,38 +15,42 @@ def get_item_type():
 def get_weapon():
     return itemAttributes['weapon'][randomnumber(0, 22)]
 
+
 def get_armor():
     return itemAttributes['armor'][randomnumber(0, 7)]
+
 
 itemType = get_item_type()
 weaponType = get_weapon()
 armorType = get_armor()
 
+answer = ""
+
 if itemType == "weapon":
-    print("You got a " + weaponType + "!")
+    answer = "You got a " + weaponType + "!"
 
 elif itemType == "armor":
-    print("You got a " + armorType + "!")
+    answer = "You got a " + armorType + "!"
 
 elif itemType == "potion":
-    print("You got a " + itemAttributes['potion'][randomnumber(0, 25)] + " potion!")
+    answer = "You got a " + itemAttributes['potion'][randomnumber(0, 25)] + " potion!"
 
 elif itemType == "scroll":
-    print("You got a scroll of " + itemAttributes['scroll'][randomnumber(0, 20)])
+    answer = "You got a scroll of " + itemAttributes['scroll'][randomnumber(0, 20)]
 
 elif itemType == "ring":
-    print("You got a ring of " + itemAttributes['ring'][randomnumber(0, 11)])
+    answer = "You got a ring of " + itemAttributes['ring'][randomnumber(0, 11)]
 
 elif itemType == "necklace":
-    print("You got a " + itemAttributes['necklace'][randomnumber(0, 11)] + " necklace!")
+    answer = "You got a " + itemAttributes['necklace'][randomnumber(0, 11)] + " necklace!"
 
 elif itemType == "wand":
-    print("You got a wand of " + itemAttributes['wand'][randomnumber(0, 13)])
+    answer = "You got a wand of " + itemAttributes['wand'][randomnumber(0, 13)]
 
 elif itemType == "component":
-    print("You got a " + itemAttributes['component'][randomnumber(0, 15)] + " component!")
+    answer = "You got a " + itemAttributes['component'][randomnumber(0, 15)] + " component!"
 
 elif itemType == "gold":
-    print("You found " + str(randomnumber(1, 250)) + " gold!")
+    answer = "You found " + str(randomnumber(1, 250)) + " gold!"
 else:
-    print(itemType)
+    answer = itemType
