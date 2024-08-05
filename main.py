89c58dc65539bffeb168acd528ed916875fa2971
Todd, Jacob <jtodd@verisk.com>
2024-08-05 12:00:41 -0500

    Reworked the main file to give a better UI. Changed the if statements to give more functionality.

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import randomizer
-from itemTables import itemAttributes
-from randomizer import randomnumber
+from data.itemTables import itemAttributes
 
-input("What random item would you like to generate? ")
-print(randomizer.generator())
+item_types = ', '.join(itemAttributes['itemType'][:-1]) + ' or ' + itemAttributes['itemType'][-1]
+print("What random item would you like to generate?")
+print("You can choose from " + item_types + ", or type 'random' for a random item.")
+user_input = input(": ")
+
+print(randomizer.generator(user_input))
--- randomizer.py
+++ randomizer.py
@@ -1,15 +1,34 @@
 import random
-from itemTables import itemAttributes
+from data.itemTables import itemAttributes
 
 
 def randomnumber(min, max):
     return random.randint(min, max)
 
-# define a variable here that I can record the item type I got at random
 
-# Getting the peices we need here
-def get_item_type():
-    return itemAttributes['itemType'][randomnumber(0, 8)]
+def get_item_type(user_input):
+    if user_input == "random":
+        return itemAttributes['itemType'][randomnumber(0, 8)]
+    elif user_input == "weapon":
+        return "weapon"
+    elif user_input == "armor":
+        return "armor"
+    elif user_input == "potion":
+        return "potion"
+    elif user_input == "scroll":
+        return "scroll"
+    elif user_input == "ring":
+        return "ring"
+    elif user_input == "necklace":
+        return "necklace"
+    elif user_input == "wand":
+        return "wand"
+    elif user_input == "component":
+        return "component"
+    elif user_input == "gold":
+        return "gold"
+    else:
+        return "I'm sorry, I didn't understand that."
 
 
 def get_weapon():
@@ -20,14 +39,11 @@ def get_armor():
     return itemAttributes['armor'][randomnumber(0, 7)]
 
 
-itemType = get_item_type()
-weaponType = get_weapon()
-armorType = get_armor()
-answer = "..."
-
-
-def generator():
-    global itemType, weaponType, armorType, answer
+def generator(user_input):
+    itemType = get_item_type(user_input)
+    weaponType = get_weapon()
+    armorType = get_armor()
+    answer = "..."
 
     if itemType == "weapon":
         answer = "You got a " + weaponType + "!"
