8649de524e6caacd525b28f7967976440b8eec15
Todd, Jacob <jtodd@verisk.com>
2024-08-13 17:03:03 -0500

    Haha I broke it trying to refine the generator function.

--- randomizer.py
+++ randomizer.py
@@ -9,7 +9,7 @@ def randomnumber(min, max):
 def get_item_type(user_input):
     item_types = ['weapon', 'armor', 'potion', 'scroll', 'ring', 'necklace', 'wand', 'component', 'gold']
     if user_input in item_types:
-        return user_input
+        return generator(user_input)
     elif user_input == "random":
         return itemAttributes['itemType'][randomnumber(0, 8)]
     else:
@@ -33,29 +33,30 @@ def generator(user_input):
     if itemType == "weapon":
         answer = "You got a " + weaponType + "!"
 
-    elif itemType == "armor":
+    if itemType == "armor":
         answer = "You got a " + armorType + "!"
 
-    elif itemType == "potion":
+    if itemType == "potion":
         answer = "You got a " + itemAttributes['potion'][randomnumber(0, 25)] + " potion!"
 
-    elif itemType == "scroll":
+    if itemType == "scroll":
         answer = "You got a scroll of " + itemAttributes['scroll'][randomnumber(0, 20)]
 
-    elif itemType == "ring":
+    if itemType == "ring":
         answer = "You got a ring of " + itemAttributes['ring'][randomnumber(0, 11)]
 
-    elif itemType == "necklace":
+    if itemType == "necklace":
         answer = "You got a " + itemAttributes['necklace'][randomnumber(0, 11)] + " necklace!"
 
-    elif itemType == "wand":
+    if itemType == "wand":
         answer = "You got a wand of " + itemAttributes['wand'][randomnumber(0, 13)]
 
-    elif itemType == "component":
+    if itemType == "component":
         answer = "You got a " + itemAttributes['component'][randomnumber(0, 15)] + " component!"
 
-    elif itemType == "gold":
+    if itemType == "gold":
         answer = "You found " + str(randomnumber(1, 250)) + " gold!"
+
     else:
         answer = itemType
 
